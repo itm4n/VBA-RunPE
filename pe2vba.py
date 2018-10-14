600afc81a70921e2c1ca5d62468f42d9a231f568
Clément LABRO <30777390+itm4n@users.noreply.github.com>
2018-10-14 13:25:03 +0200

    Improved the PE embedding method

--- pe2vba.py
+++ pe2vba.py
@@ -3,41 +3,70 @@ from os import system, remove
 from os.path import isfile
 import argparse
 
-MAX_PROC_SIZE = 100 # Nbr of lines per procedure 
-MAX_LINE_SIZE = 10 # Nbr of bytes per lines 
+MAX_PROC_SIZE = 50 # Nbr of lines per procedure 
+MAX_LINE_SIZE = 50 # Nbr of bytes per line
+
+def is_printable(c):
+  # All characters from SPACE to ~ are printable ASCII.
+  # However, we want to avoid '"' 
+  if c >= 0x20 and c < 0x7F and c != 0x22:
+    return True
+  else:
+    return False
 
 def pe_to_vba(pe):
-  result = "" 
+  block = "" 
+  line = ""
   ba = bytearray(pe)
   
   blocks = []
-  cnt_bytes = 0
-  cnt_lines = 0 
+  cnt_bytes_current_line = 0
+  cnt_lines_current_block = 0
+  cnt_bytes_total = 0 
+  prev_char_was_printable = False
   
   for b in ba:
   
-    if cnt_lines == 0:
+    if cnt_lines_current_block == 0:
       # Start a new block 
-      result = "    strPE = \"\"\n"   
-      cnt_lines += 1 
-    if cnt_bytes == 0:
+      block = "    strPE = \"\"\n"   
+      cnt_lines_current_block += 1 
+    if cnt_bytes_current_line == 0:
       # Start a new line 
-      result += "    strPE = strPE "
+      line = "strPE"
     
-    result += " + " + "chr(&h" + format(b, "02x") + ")"  
-    cnt_bytes += 1 # We added a byte so increment the counter 
+    if is_printable(b):
+      if prev_char_was_printable:
+        line += chr(b)
+      else:
+        line = "B(%s, \"%s" % (line, chr(b))
+      prev_char_was_printable = True
+    else:
+      if prev_char_was_printable:
+        line += "\")"
+      line = "A(%s, %s)" % (line, str(b))
+      prev_char_was_printable = False
+    
+    cnt_bytes_current_line += 1 # We added a byte so increment the counter 
+    cnt_bytes_total += 1 # We treated one more byte in the overall file 
 
-    if cnt_bytes == MAX_LINE_SIZE:
-      # If we reach the end of a line, add a LINE FEED 
-      result += "\n"
-      cnt_bytes = 0
-      cnt_lines += 1
-    
-    if cnt_lines == MAX_PROC_SIZE:
-      # If we reach max procedure size 
-      cnt_lines = 0 # Reset the number of lines 
-      cnt_bytes = 0 # Reset the number of bytes per line 
-      blocks.append(result) # Append the current block to the list of procedudes 
+    # If we reach the max number of bytes per line or the end of the array 
+    # then end the current line.
+    if cnt_bytes_current_line == MAX_LINE_SIZE or cnt_bytes_total == len(ba):
+      if prev_char_was_printable:
+        block += "    strPE = %s\")\n" % (line)
+      else:
+        block += "    strPE = %s\n" % (line)
+      prev_char_was_printable = False # Must reset this flag for each new line 
+      cnt_bytes_current_line = 0
+      cnt_lines_current_block += 1
+
+    # If we reach the max number of lines per block or the end of the array
+    # then end the current block. 
+    if cnt_lines_current_block == MAX_PROC_SIZE or cnt_bytes_total == len(ba):
+      cnt_lines_current_block = 0 # Reset the number of lines 
+      cnt_bytes_current_line = 0 # Reset the number of bytes per line 
+      blocks.append(block) # Append the current block to the list of procedudes 
   
   # Create a 'Sub' for each block  
   proc = ""
@@ -90,8 +119,5 @@ def main():
 
 if __name__ == '__main__':
   main() 
-
-
-
 
 
